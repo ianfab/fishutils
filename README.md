8630f8b5fac19eb7fa9bd0656789a35a0fc64f71
Fabian Fichter <ianfab@users.noreply.github.com>
2019-11-01 15:52:39 +0100

    Migrate to python3 and refactor code

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Auto-apply parameter tweaks to [Stockfish](https://github.com/official-stockfish/Stockfish) or [multi-variant Stockfish](https://github.com/ddugovic/Stockfish).
 
 ## Usage
-Run `python fishutils.py -h` to get info on required and optional arguments. See the [Wiki](https://github.com/ianfab/fishutils/wiki) for a simple example on how to use the script.
+Run `python3 fishutils.py -h` to get info on required and optional arguments. See the [Wiki](https://github.com/ianfab/fishutils/wiki) for a simple example on how to use the script.
 
-The main functionality of the script is to automatically apply SPSA tuning results from [fishtest](https://github.com/glinscott/fishtest) or [multi-variant fishtest](https://github.com/ianfab/fishtest) to your Stockfish repository. To do this, copy the results of a SPSA tuning session to a file and run `python fishutils.py -s /path/to/stockfish/src/ -i /path/to/tuning_results.txt`, or copy the the tuning results to the clipboard and run `python fishutils.py -s /path/to/stockfish/src/` and then insert them.
+The main functionality of the script is to automatically apply SPSA tuning results from [fishtest](https://github.com/glinscott/fishtest) or [multi-variant fishtest](https://github.com/ianfab/fishtest) to your Stockfish repository. To do this, copy the results of a SPSA tuning session to a file and run `python3 fishutils.py -s /path/to/stockfish/src/ -i /path/to/tuning_results.txt`, or copy the the tuning results to the clipboard and run `python3 fishutils.py -s /path/to/stockfish/src/` and then insert them.
 
 An input line for the script (taken from fishtest tuning results) can, e.g., look like:
 `param: mLever[4], best: 27.00, start: 17.00, min: -100.00, max: 200.00, c 41.714131, a 223.069148`. The script then changes the value in the source code according to the given input line, in this example changing the middlegame value of `Lever[4]` from 17 to 27.
--- fishutils.py
+++ fishutils.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # fishutils is a script for semi-automatic code changes in Stockfish.
-# Copyright (C) 2017 Fabian Fichter
+# Copyright (C) 2017-2019 Fabian Fichter
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,9 +22,9 @@ import re
 import argparse
 import logging
 import traceback
+import parser
 from math import floor, ceil
 from difflib import unified_diff
-import parser
 
 MODES = ("spsa", "function", "align")
 ROUNDING = {'round': round, 'ceil': ceil, 'floor': floor}
@@ -32,21 +32,24 @@ LOGGING = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 'WARNING': logging.WARN
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--src-dir", help="path to directory of Stockfish source code", type=str, required=True)
-    parser.add_argument("-i", "--input-file", help="path to input file (e.g., containing tuning results)", type=str)
-    parser.add_argument("-m", "--mode", help="mode. Default: spsa", choices=MODES, default="spsa")
-    mutex_group = parser.add_mutually_exclusive_group()
+    arg_parser = argparse.ArgumentParser(description="Auto-apply parameter tweaks to source code.")
+    arg_parser.add_argument("-s", "--src-dir", help="path to directory of Stockfish source code", type=str,
+                            required=True)
+    arg_parser.add_argument("-i", "--input-file", help="path to input file (e.g., containing tuning results)", type=str)
+    arg_parser.add_argument("-m", "--mode", help="mode. Default: spsa", choices=MODES, default="spsa")
+    mutex_group = arg_parser.add_mutually_exclusive_group()
     mutex_group.add_argument("-d", "--dry-run", help="show changes, do not write to files", action="store_true")
     mutex_group.add_argument("--low-memory", help="do not keep all files in memory", action="store_true")
-    parser.add_argument("-r", "--rounding", help="rounding function. Default: round", choices=ROUNDING.keys(), default="round")
-    parser.add_argument("-l", "--log-level", help="logging level. Default: WARNING", choices=LOGGING.keys(), default="WARNING")
-    return parser.parse_args()
+    arg_parser.add_argument("-r", "--rounding", help="rounding function. Default: round", choices=ROUNDING.keys(),
+                            default="round")
+    arg_parser.add_argument("-l", "--log-level", help="logging level. Default: WARNING", choices=LOGGING.keys(),
+                            default="WARNING")
+    return arg_parser.parse_args()
 
 
 def get_index(enum, entry):
     """Calculates enum value by counting commata."""
-    m = re.search(entry + "\s*=\s*(?P<v>(-)?\d+)", enum)
+    m = re.search(entry + r"\s*=\s*(?P<v>(-)?\d+)", enum)
     if m is not None:
         return int(m.group("v"))
     else:
@@ -72,9 +75,9 @@ def find_index(s, l):
     for d in range(len(l)):
         for i in range(l[d]):
             pos_new = s.find(",", pos + 1)
-            while s.count("{", 0, pos_new+1) - s.count("}", 0, pos_new+1) > d + 1:
+            while s.count("{", 0, pos_new + 1) - s.count("}", 0, pos_new + 1) > d + 1:
                 pos_new = s.find("}", pos_new + 1)
-            while s.count("(", pos, pos_new+1) - s.count(")", pos, pos_new+1) > 0:
+            while s.count("(", pos, pos_new + 1) - s.count(")", pos, pos_new + 1) > 0:
                 pos_new = s.find(")", pos_new + 1)
             pos = s.find(",", pos_new)
     return pos
@@ -112,7 +115,8 @@ class SourceFile(object):
     def diff(self):
         assert self.in_memory, "File has to be in memory to show diff."
         s = str()
-        for line in unified_diff(self.orig.splitlines(True), self.curr.splitlines(True), fromfile=self.path, tofile=self.path):
+        for line in unified_diff(self.orig.splitlines(True), self.curr.splitlines(True), fromfile=self.path,
+                                 tofile=self.path):
             s += line
         return s
 
@@ -128,42 +132,46 @@ class Repository(object):
 
     The path to the repository is a required argument.
     """
+
     def __init__(self, path, in_memory=True):
         self.src_path = os.path.abspath(path)
         self.in_memory = in_memory
-        self.file_paths = [os.path.join(self.src_path, f) for f in os.listdir(self.src_path) if is_source(os.path.join(self.src_path, f))]
+        self.file_paths = [os.path.join(self.src_path, f) for f in os.listdir(self.src_path) if
+                           is_source(os.path.join(self.src_path, f))]
         self.files = list()
         for f_path in self.file_paths:
             self.files.append(SourceFile(f_path, self.in_memory))
 
-    def search_def(self, varname, enum=False):
+    def search_def(self, var_name, enum=False):
         """
         Searches for definition of variable or enum value.
 
-        Returns a regex match object or None.
+        Returns a regex match object and a file handle.
         """
 
-        for f in self.files:
-            s = f.read()
+        for file in self.files:
+            s = file.read()
             if enum:
-                pattern = "^\s*enum\s*"+r"[\w\s:]*{([^{}]*?(\s|,))?"+varname+"(\s|,|}).*?;"
+                pattern = r"^\s*enum\s*" + r"[\w\s:]*{([^{}]*?(\s|,))?" + var_name + r"(\s|,|}).*?;"
             else:
-                pattern = "^([\w \t]*\s+|)(?P<type>\w+)\s+"+varname+"((\[|\s)?(\[[^=;\\\{\}\[\]]*\])*[ \t]*)?=(?P<def>(?!=)[^;]*?);"
+                pattern = (r"^([\w \t]*\s+|)(?P<type>\w+)\s+"
+                           + var_name
+                           + r"((\[|\s)?(\[[^=;\\\{\}\[\]]*\])*[ \t]*)?=(?P<def>(?!=)[^;]*?);")
             m = re.search(pattern, s, re.DOTALL | re.MULTILINE)
             if m is not None:
-                return m, f
+                return m, file
 
         return None, None
 
     def parse_indices(self, s):
         """Returns an array of integers from string of index accesses ('[1][2][3]' -> [1, 2, 3])."""
-        l = []
+        indices = []
         while s != "":
             p1 = s.find('[')
             p2 = s.find(']')
             if p1 == -1 or p2 == -1 or p1 + 1 >= p2:
                 break
-            i = s[p1+1:p2]
+            i = s[p1 + 1:p2]
             try:
                 i = int(i)
             except ValueError:
@@ -172,9 +180,9 @@ class Repository(object):
                     i = get_index(m.group(), i)
                 else:
                     raise Exception("Index %s is neither int nor enum." % i)
-            l.append(i)
-            s = s[p2+1:]
-        return l
+            indices.append(i)
+            s = s[p2 + 1:]
+        return indices
 
     def process_spsa_match(self, match_input, f_round):
         """
@@ -189,9 +197,9 @@ class Repository(object):
         else:
             name = match_input.group("name")
             prefix = "v"
-        fullname = match_input.group("name")+match_input.group("indices")
+        fullname = match_input.group("name") + match_input.group("indices")
         indices = self.parse_indices(match_input.group("indices"))
-        match_definition, f = self.search_def(name)
+        match_definition, file = self.search_def(name)
         if match_definition is None:
             logging.error("%s: Definition not found." % fullname)
             return
@@ -202,11 +210,11 @@ class Repository(object):
         assert (match_definition.group("type") == "Score") == bool(prefix in ("m", "e"))
 
         if prefix in ("m", "e"):
-            pattern = "^(?:(?!#|//).)*?(S|make_score)\((?P<m>\s*[-\d]+)\s*,(?P<e>\s*[-\d]+)\s*\)"
+            pattern = r"^(?:(?!#|//).)*?(S|make_score)\((?P<m>\s*[-\d]+)\s*,(?P<e>\s*[-\d]+)\s*\)"
         elif match_definition.group("type") == "Value":
-            pattern = "^(?:(?!#|//).)*?(((V|Value)\((?P<v>\s*[-\d]+)\s*\))|(?P<enum>\w+))"
-        elif match_definition.group("type") in ("int"):
-            pattern = "^(?:(?!#|//).)*?((?P<v>[ \t]*[-\d]+)|(?P<enum>\w+))"
+            pattern = r"^(?:(?!#|//).)*?(((V|Value)\((?P<v>\s*[-\d]+)\s*\))|(?P<enum>\w+))"
+        elif match_definition.group("type") == "int":
+            pattern = r"^(?:(?!#|//).)*?((?P<v>[ \t]*[-\d]+)|(?P<enum>\w+))"
         else:
             logging.error("%s: Unknown type %s." % (fullname, match_definition.group("type")))
             return
@@ -222,91 +230,94 @@ class Repository(object):
             prefix = 'v'
             i = 0
             logging.debug("%s: Replacing defining enum '%s' instead of array entry itself." % (fullname, enum_name))
-            match_definition, f = self.search_def(enum_name, enum=True)
+            match_definition, file = self.search_def(enum_name, enum=True)
             if match_definition is not None:
-                logging.debug("Enum definition found in file %s: %s" % (f.path, match_value.group(0)))
-                match_value = re.search(enum_name+"\s*=\s*(?P<v>(-)?\d+)", match_definition.group(0), re.DOTALL | re.MULTILINE)
+                logging.debug("Enum definition found in file %s: %s" % (file.path, match_value.group(0)))
+                match_value = re.search(enum_name + r"\s*=\s*(?P<v>(-)?\d+)", match_definition.group(0),
+                                        re.DOTALL | re.MULTILINE)
                 assert match_value is not None
             else:
                 logging.error("Replacing %s failed. Definition '%s' can not be handled." % (fullname, enum_name))
                 return
 
-        if match_input.group("value_old") is not None and int(match_value.group(prefix)) != int(float(match_input.group("value_old"))):
+        if match_input.group("value_old") is not None and int(match_value.group(prefix)) != int(
+                float(match_input.group("value_old"))):
             logging.warning("%s: Value in file differs from start value: %s !=  %s" %
                             (fullname, int(match_value.group(prefix)), int(float(match_input.group("value_old")))))
 
         if len(match_value.group(prefix)) < len(new_val):
             logging.info("%s: Code lines might be not aligned." % fullname)
 
-        fstr =  match_definition.string[:match_definition.start()+i+match_value.start(prefix)] \
-              + new_val.rjust(len(match_value.group(prefix))) \
-              + match_definition.string[match_definition.start()+i+match_value.end(prefix):]
+        file_content = (match_definition.string[:match_definition.start() + i + match_value.start(prefix)]
+                        + new_val.rjust(len(match_value.group(prefix)))
+                        + match_definition.string[match_definition.start() + i + match_value.end(prefix):])
 
-        f.write(fstr)
+        file.write(file_content)
 
     def process_function_match(self, match_input, f_round):
         varname, f_string = match_input.group('name'), match_input.group('func')
-        match_definition, f = self.search_def(varname)
+        match_definition, file = self.search_def(varname)
         if match_definition is None:
             logging.error("%s: Definition not found." % varname)
             return
 
         try:
-            func = eval(parser.expr('lambda x: '+f_string).compile())
-        except:
+            func = eval(parser.expr('lambda x: ' + f_string).compile())
+        except Exception:
             logging.error("%s: Function '%s' could not be parsed." % (varname, f_string))
-            return
+            raise
 
         def_string = match_definition.group('def')
         pattern = re.compile(r'\d+')
         try:
             new_def = pattern.sub(lambda x: str(int(f_round(func(float(x.group()))))), def_string)
-        except:
+        except Exception:
             logging.error("%s: Failed to apply function '%s'." % (varname, f_string))
-            return
+            raise
 
-        fstr =  match_definition.string[:match_definition.start('def')] \
-              + new_def \
-              + match_definition.string[match_definition.end('def'):]
+        file_content = (match_definition.string[:match_definition.start('def')]
+                        + new_def
+                        + match_definition.string[match_definition.end('def'):])
 
-        f.write(fstr)
+        file.write(file_content)
 
     def align_array(self, match_input, _):
-        varname = match_input.group('name')
-        match_definition, f = self.search_def(varname)
+        var_name = match_input.group('name')
+        match_definition, file = self.search_def(var_name)
         if match_definition is None:
-            logging.error("%s: Definition not found." % varname)
+            logging.error("%s: Definition not found." % var_name)
             return
         current = match_definition.group(0)
         previous = ""
         while current != previous:
             previous = current
             current = self.align(current)
-        fstr =  match_definition.string[:match_definition.start()] \
-            + current \
-            + match_definition.string[match_definition.end():]
+        file_content = (match_definition.string[:match_definition.start()]
+                        + current
+                        + match_definition.string[match_definition.end():])
 
-        f.write(fstr)
+        file.write(file_content)
 
     @staticmethod
     def align(s):
         lines = s.splitlines()
         format_patterns = []
         for line in lines:
-            format_patterns.append(re.sub('[^{}\(\),]*', '', line))
+            format_patterns.append(re.sub(r'[^{}(),]*', '', line))
 
-        for i in xrange(max(len(lines) - 1, 0)):
-            if (len(format_patterns[i]) > 0 and format_patterns[i].rstrip(',') == format_patterns[i+1].rstrip(',') \
-                and (format_patterns[i][0] != '{' or len(os.path.commonprefix([lines[i], lines[i+1]])) >= lines[i].find(format_patterns[i][0]))):
+        for i in range(max(len(lines) - 1, 0)):
+            if (len(format_patterns[i]) > 0 and format_patterns[i].rstrip(',') == format_patterns[i + 1].rstrip(',')
+                    and (format_patterns[i][0] != '{' or len(os.path.commonprefix([lines[i], lines[i + 1]])) >= lines[
+                        i].find(format_patterns[i][0]))):
                 # Align adjacent lines
                 pos_old = -1
-                for j in format_patterns[i][:len(os.path.commonprefix([format_patterns[i], format_patterns[i+1]]))]:
-                    pos0 = lines[i  ].find(j, pos_old + 1)
-                    pos1 = lines[i+1].find(j, pos_old + 1)
+                for j in format_patterns[i][:len(os.path.commonprefix([format_patterns[i], format_patterns[i + 1]]))]:
+                    pos0 = lines[i].find(j, pos_old + 1)
+                    pos1 = lines[i + 1].find(j, pos_old + 1)
                     if pos0 < pos1:
-                        lines[i  ] = lines[i  ][:pos_old+1] + (pos1 - pos0) * ' ' + lines[i  ][pos_old+1:]
+                        lines[i] = lines[i][:pos_old + 1] + (pos1 - pos0) * ' ' + lines[i][pos_old + 1:]
                     elif pos0 > pos1:
-                        lines[i+1] = lines[i+1][:pos_old+1] + (pos0 - pos1) * ' ' + lines[i+1][pos_old+1:]
+                        lines[i + 1] = lines[i + 1][:pos_old + 1] + (pos0 - pos1) * ' ' + lines[i + 1][pos_old + 1:]
                     pos_old = max(pos0, pos1)
 
         return "\n".join(lines)
@@ -325,11 +336,11 @@ class Repository(object):
 
 
 class InputParser(object):
-    def __init__(self, input_strings=[], repo=None, src_dir=None):
+    def __init__(self, input_strings=None, repo=None, src_dir=None):
         self.process_method = None
         self.regex_pattern = None
         self.help_text = None
-        self.input_strings = input_strings
+        self.input_strings = input_strings if input_strings else []
         assert repo is not None or src_dir is not None
         self.repo = repo if repo is not None else Repository(src_dir)
 
@@ -337,7 +348,10 @@ class InputParser(object):
         assert self.help_text is not None
         print(self.help_text)
         while True:
-            s = raw_input()
+            try:
+                s = input()
+            except EOFError:
+                sys.exit(1)
             if not s:
                 break
             self.input_strings.append(s)
@@ -352,8 +366,8 @@ class InputParser(object):
             if match_input:
                 try:
                     self.process_match(match_input, f_round)
-                except:
-                    logging.error("Failed to process input line '%s':\n%s" % (line, traceback.format_exc()))
+                except Exception:
+                    logging.exception("Failed to process input line '%s'" % line)
             else:
                 logging.warning("Skipping invalid input line: '%s'." % line)
 
@@ -373,11 +387,13 @@ class InputParser(object):
 
 class ResultParser(InputParser):
     """Parser object for processing result strings of SPSA tuning sessions."""
+
     def __init__(self, *args, **kwargs):
         super(ResultParser, self).__init__(*args, **kwargs)
         self.process_method = self.repo.process_spsa_match
-        self.regex_pattern = "^\s*(param|Parameter): (?P<name>\w*)(?P<indices>[[\]\w\s]*), (best|theta): (?P<value_new>[\w.-]*)" \
-                             "(, start: (?P<value_old>[\w.-]*))?"
+        self.regex_pattern = r"^\s*(param|Parameter): (?P<name>\w*)(?P<indices>[[\]\w\s]*), (best|theta):" \
+                             r" (?P<value_new>[\w.-]*)" \
+                             r"(, start: (?P<value_old>[\w.-]*))?"
         self.help_text = "Give SPSA tuning results as reported by fishtest.\nQuit input by empty line.\nInput:"
 
 
@@ -385,8 +401,9 @@ class FunctionParser(InputParser):
     def __init__(self, *args, **kwargs):
         super(FunctionParser, self).__init__(*args, **kwargs)
         self.process_method = self.repo.process_function_match
-        self.regex_pattern = "^\s*(?P<name>\S+)\s*;\s*(?P<func>.+?)\s*$"
-        self.help_text = "Give array and a formula f(x) separated by a semicolon.\nExample: 'razor_margin;2*x' doubles the razoring margins.\n" \
+        self.regex_pattern = r"^\s*(?P<name>\S+)\s*;\s*(?P<func>.+?)\s*$"
+        self.help_text = "Give array and a formula f(x) separated by a semicolon.\n" \
+                         "Example: 'razor_margin;2*x' doubles the razoring margins.\n" \
                          "Quit input by empty line.\nInput:"
 
 
@@ -394,7 +411,7 @@ class AlignParser(InputParser):
     def __init__(self, *args, **kwargs):
         super(AlignParser, self).__init__(*args, **kwargs)
         self.process_method = self.repo.align_array
-        self.regex_pattern = "^\s*(?P<name>\S+)\s*$"
+        self.regex_pattern = r"^\s*(?P<name>\S+)\s*$"
         self.help_text = "Give the names of the arrays that are to be aligned, with one name per line.\n" \
                          "Quit input by empty line.\nInput:"
 
@@ -420,10 +437,10 @@ def main(args):
     else:
         assert False, "This should never be reached. Check definition of modes."
 
-    parser = parser_class(input_strings=input_strings, repo=repo)
+    input_parser = parser_class(input_strings=input_strings, repo=repo)
     if not input_strings:
-        parser.user_input()
-    parser.process(dry_run=args.dry_run, f_round=ROUNDING[args.rounding])
+        input_parser.user_input()
+    input_parser.process(dry_run=args.dry_run, f_round=ROUNDING[args.rounding])
 
 
 if __name__ == "__main__":
